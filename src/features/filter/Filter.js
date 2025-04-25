0d8c1f7a94c7dd4766e0b13e4aa34d1942200534
clmilholland <luke12milholland@gmail.com>
2025-04-25 12:11:41 -0600

    created Filter component

---
+++ src/features/filter/Filter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { selectAllArtworkIDs, selectALLArtwork } from "../artwork/gatherArtworkSlice";
+
+const Filter = () => {
+
+}
+
+export default Filter;
